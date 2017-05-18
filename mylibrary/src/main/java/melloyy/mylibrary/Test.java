f1d2ca2393b4b45a8561448321c32ea7d7ae642a
SJTY-YY <896201545@qq.com>
2017-05-18 17:09:09 +0800

    测试自己创建一个android studio在线依赖compile

---
+++ mylibrary/src/main/java/melloyy/mylibrary/Test.java
@@ -0,0 +1,15 @@
+package melloyy.mylibrary;
+
+/**测试类
+ * Created by since on 2017/5/18.
+ */
+
+public class Test {
+    public String Test (int i){
+        if ( 0 == i){
+            return "0";
+        }else {
+            return "1";
+        }
+    }
+}
